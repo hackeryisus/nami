2a6897b8b8ae6c39dbf94434d2e0b457d759a0eb
jemandez <jeman.400@gmail.com>
2017-03-11 11:42:04 -0600

    Esqueleto para el acerca de nosotros

--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,9 @@ router.get('/tutor', function(req, res, next) {
   res.render('tutor', { title: 'Tutor' });
 });
 
+/* GET home page. */
+router.get('/nosotros', function(req, res, next) {
+  res.render('about', { title: 'Acerca de' });
+});
+
 module.exports = router;
