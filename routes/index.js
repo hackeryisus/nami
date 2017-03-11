fb96090b459794b0dea7b241aed97cd42f9cfa7f
jemandez <jeman.400@gmail.com>
2017-03-10 23:14:48 -0600

    Arreglos de menu

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Inicio' });
+});
+
+/* GET home page. */
+router.get('/tutor', function(req, res, next) {
+  res.render('tutor', { title: 'Tutor' });
 });
 
 module.exports = router;
